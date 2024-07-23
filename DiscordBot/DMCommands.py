66a6f7e3a85b06e95da8b7869d08c95110ed4201
KAMISENHEIMER <113205388+KAMISENHEIMER@users.noreply.github.com>
2024-07-23 15:23:03 -0500

    added DM character info command, and ensured DM completequest command has correct values when used

--- DiscordBot/DMCommands.py
+++ DiscordBot/DMCommands.py
@@ -44,6 +44,24 @@ class DMOnly(commands.Cog):
 
     @commands.command()
     @commands.has_role('DM')
+    async def info(self, ctx, id):
+        """tells information about a character"""
+        info = getCharacterInfo(id)
+
+        # builds response
+        response = "ID: " + str(info[1]) + "\nName: " + info[0] + "\nClass: " + info[3] + " " + info[2] + "\nRace: " + \
+                   info[4] + "\nLevel: " + str(info[5]) + "\nXP: "
+        # reformats XP
+        if info[8].is_integer():
+            response = response + str(int(info[8]))
+        else:
+            response = response + str(info[8])
+        response = response + "/" + str(info[5] + 1) + "\nQuests Completed: " + str(info[7]) + "\nBalance: " + str(
+            info[6]) + " GP"
+        await ctx.send(response)
+
+    @commands.command()
+    @commands.has_role('DM')
     async def updatecharacter(self, ctx, character_id=None, stat=None, new_info=None):
         """updates information about a character"""
         if character_id and stat and new_info:
@@ -59,24 +77,30 @@ class DMOnly(commands.Cog):
 
     @commands.command()
     @commands.has_role('DM')
-    async def completequest(self, ctx, questLevel, *ids):
+    async def completequest(self, ctx, questLevel=None, *ids: int):
         """gives players XP based on completed quest"""
-        response = "Quest Completed"
-        for id in ids:
-            info = getCharacterInfo(id)
-            charLevel = info[5]
-            XP = info[8]
-            givenXP = (int(questLevel) - charLevel) / 2 + 1
-            newXP = XP + givenXP
-            if newXP >= charLevel + 1:
-                charLevel += 1
-                newXP = 0
-                userID = findUserFromCharacter(id)
-                discordID = findDiscordID(userID)
-                response += "\n <@" + str(discordID) + ">, " + info[0] + " has leveled up, and is now level " + str(charLevel)
-            questsCompleted = getCharacterInfo(id)[7] + 1
-            if not updateCharacter("UPDATE characters SET XP = " + str(newXP) + ", quests_completed = " + str(
-                    questsCompleted) + ", lvl = " + str(charLevel) + " WHERE id = " + str(id)):
-                response = "Failed to Complete Quest"
+
+        # makes sure all parameters exist
+        if questLevel and ids:
+
+            response = "Quest Completed"
+            for id in ids:
+                info = getCharacterInfo(id)
+                charLevel = info[5]
+                XP = info[8]
+                givenXP = (int(questLevel) - charLevel) / 2 + 1
+                newXP = XP + givenXP
+                if newXP >= charLevel + 1:
+                    charLevel += 1
+                    newXP = 0
+                    userID = findUserFromCharacter(id)
+                    discordID = findDiscordID(userID)
+                    response += "\n <@" + str(discordID) + ">, " + info[0] + " has leveled up, and is now level " + str(charLevel)
+                questsCompleted = getCharacterInfo(id)[7] + 1
+                if not updateCharacter("UPDATE characters SET XP = " + str(newXP) + ", quests_completed = " + str(
+                        questsCompleted) + ", lvl = " + str(charLevel) + " WHERE id = " + str(id)):
+                    response = "Failed to Complete Quest"
+        else:
+            response = "Usage: !completequest [Quest Level] [IDs of characters]"
 
         await ctx.send(response)
